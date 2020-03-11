acc8c8fccf5e8a986f569657ee8bea83ea746a09
shubham.s <shubham.s@TO-64NL882-D.corp.techouts.local>
2020-03-11 14:58:23 +0530

    Added to return all details

--- OAuthTest/src/main/java/com/nt/controller/WelcomeController.java
+++ OAuthTest/src/main/java/com/nt/controller/WelcomeController.java
@@ -9,23 +9,24 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WelcomeController {
-	
+
 	/*@RequestMapping("/user")
 	@ResponseBody
 	public String display() {
 		return "<h1 style='color:red;text-align:center'>Home Page</h1>";
 	}*/
-	
+
 	@SuppressWarnings("unchecked")
 	@RequestMapping("/user")
-	 @ResponseBody
-	    public String user(OAuth2Authentication authentication) {
-	        LinkedHashMap<String, Object> properties = (LinkedHashMap<String, Object>) authentication.getUserAuthentication().getDetails();
-	        System.out.println(properties);
-	        return "<h1 align='center'>Welcome "+(String)properties.get("email")+"</h1>";
-	    }
+	@ResponseBody
+	public String user(OAuth2Authentication authentication) {
+		LinkedHashMap<String, Object> properties = (LinkedHashMap<String, Object>) authentication
+				.getUserAuthentication().getDetails();
+		System.out.println(properties);
+		return "<h1 align='center'>Welcome " + (String) properties.get("email") + "</h1>"
+				+ "<h1 align='center'>Welcome " + (String) properties.get("given_name") + "</h1>"
+				+ "<h1 align='center'>Welcome " + (String) properties.get("family_name") + "</h1>"
+				+ "<img src="+properties.get("picture")+">";
+	}
 
 }
-
-
-
